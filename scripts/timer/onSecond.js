759afe650f20790481f1fe9079d6ef24457224f2
smell-of-curry <75345244+smell-of-curry@users.noreply.github.com>
2021-09-06 21:03:59 -0400

    fixed it not setting chatsSent objective first

--- scripts/timer/onSecond.js
+++ scripts/timer/onSecond.js
@@ -5,6 +5,7 @@ var second = 0
 export function onSecond() {
     second++
     if (second >= 10) {
+        Commands.run(`scoreboard objectives add chatsSent dummy`)
         Commands.run(`scoreboard objectives remove chatsSent`)
         Commands.run(`scoreboard objectives add chatsSent dummy`)
         Commands.run(`scoreboard players add @a chatsSent 0`)
